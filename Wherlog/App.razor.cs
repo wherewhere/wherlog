fb9af02b5030fa73f8f75807eefa5f2fe8bb9d6e
wherewhere <qq2518766683@outlook.com>
2024-05-27 00:21:53 +0800

    尝试添加国际化

---
+++ Wherlog/App.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FluentUI.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Wherlog
+{
+    public partial class App
+    {
+        public static Task Main(string[] args)
+        {
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services
+                   .AddLocalization()
+                   .AddFluentUIComponents();
+
+            return builder.Build().RunAsync();
+        }
+    }
+}
