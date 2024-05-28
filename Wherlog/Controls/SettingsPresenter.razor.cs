c5ff93fc9edbde896080b96a2461b016d5ba0a63
wherewhere <qq2518766683@outlook.com>
2024-05-29 01:02:46 +0800

    现在设置卡片可以自适应了
    完善设置信息选项

---
+++ Wherlog/Controls/SettingsPresenter.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Wherlog.Controls
+{
+    public partial class SettingsPresenter : FluentComponentBase
+    {
+        /// <summary>
+        /// Gets or sets the Header.
+        /// </summary>
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        [Parameter]
+        public RenderFragment Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the icon on the left.
+        /// </summary>
+        [Parameter]
+        public RenderFragment Icon { get; set; }
+
+        /// <summary>
+        /// Gets or sets the icon that is shown when IsClickEnabled is set to true.
+        /// </summary>
+        [Parameter]
+        public RenderFragment ActionIcon { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content of a ContentControl.
+        /// </summary>
+        [Parameter]
+        public RenderFragment ActionContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tooltip of the ActionIcon.
+        /// </summary>
+        [Parameter]
+        public string ActionIconToolTip { get; set; } = "More";
+    }
+}
