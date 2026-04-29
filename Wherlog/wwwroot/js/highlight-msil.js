859a8f7c25abef9b50e6deec0646fe8c1f3e0c6f
where where <qq2518766683@outlook.com>
2026-04-29 13:51:49 +0800

    更新 MSIL 语法高亮

--- Wherlog/wwwroot/js/highlight-msil.js
+++ Wherlog/wwwroot/js/highlight-msil.js
@@ -5,250 +5,391 @@ Description: Microsoft Intermediate Language
 Category: assembler
 */
 
+/** @type {import("highlight.js").LanguageFn} */
 function LanguageFn(hljs) {
+    const DECL_KEYWORDS = [
+        '.addon',
+        '.assembly',
+        '.class',
+        '.corflags',
+        '.custom',
+        '.data',
+        '.emitbyte',
+        '.entrypoint',
+        '.event',
+        '.export',
+        '.field',
+        '.file',
+        '.fire',
+        '.get',
+        '.hash',
+        '.imagebase',
+        '.interfaceimpl',
+        '.language',
+        '.line',
+        '.locale',
+        '.locals',
+        '.maxstack',
+        '.method',
+        '.module',
+        '.mresource',
+        '.mscorlib',
+        '.namespace',
+        '.other',
+        '.override',
+        '.pack',
+        '.param',
+        '.permission',
+        '.permissionset',
+        '.property',
+        '.publickey',
+        '.publickeytoken',
+        '.removeon',
+        '.set',
+        '.size',
+        '.stackreserve',
+        '.subsystem',
+        '.try',
+        '.typedef',
+        '.typelist',
+        '.ver',
+        '.vtable',
+        '.vtentry',
+        '.vtfixup',
+        '.zeroinit'
+    ];
+
     const BUILT_IN_KEYWORDS = [
+        'array',
+        'blob',
+        'blob_object',
         'bool',
-        'uint8',
+        'bstr',
+        'byvalstr',
+        'carray',
+        'cf',
         'char',
-        'unsigned',
-        'float64',
+        'class',
+        'clsid',
+        'currency',
+        'custom',
+        'date',
+        'decimal',
+        'error',
+        'filetime',
         'float32',
-        'int32',
-        'int64',
-        'native',
-        'object',
+        'float64',
+        'hresult',
+        'idispatch',
+        'interface',
+        'int',
         'int8',
         'int16',
+        'int32',
+        'int64',
+        'iunknown',
+        'lpstr',
+        'lpstruct',
+        'lptstr',
+        'lpwstr',
+        'native',
+        'object',
+        'objectref',
+        'pinned',
+        'record',
+        'safearray',
+        'storage',
+        'stream',
+        'streamed_object',
+        'stored_object',
         'string',
-        'uint64',
+        'struct',
+        'syschar',
+        'sysstring',
+        'tbstr',
+        'typedref',
+        'uint',
+        'uint8',
+        'uint16',
         'uint32',
-        'uint16'
+        'uint64',
+        'unsigned',
+        'userdefined',
+        'valuetype',
+        'variant',
+        'void'
     ];
-    const CLASS_MODIFIERS = [
-        'public',
-        'private',
-        'value',
-        'enum',
-        'interface',
-        'sealed',
+
+    const ATTR_KEYWORDS = [
         'abstract',
-        'auto',
-        'sequential',
-        'explicit',
+        'aggressiveinlining',
+        'aggressiveoptimization',
+        'amd64',
         'ansi',
-        'unicode',
-        'autochar',
-        'import',
-        'serializable',
-        'windowsruntime',
-        'nested',
-        'family',
+        'arm',
+        'arm64',
         'assembly',
-        'famandassem',
-        'famorassem',
+        'async',
+        'auto',
+        'autochar',
         'beforefieldinit',
-        'specialname',
-        'rtspecialname',
-        'flags'
-    ];
-    const FUNCTION_MODIFIERS = [
-        'static',
-        'public',
-        'private',
+        'bestfit',
+        'byreflike',
+        'callconv',
+        'cdecl',
+        'charmaperror',
+        'cil',
+        'default',
+        'enum',
+        'explicit',
+        'extended',
+        'famandassem',
         'family',
+        'famorassem',
+        'fastcall',
         'final',
-        'specialname',
-        'virtual',
-        'strict',
-        'abstract',
-        'assembly',
-        'famandassem',
-        'famorassem',
-        'privatescope',
+        'flags',
+        'forwarder',
+        'forwardref',
         'hidebysig',
-        'newslot',
-        'rtspecialname',
-        'unmanagedexp',
-        'reqsecobj',
-        'flags',
-        'pinvokeimpl'
-    ];
-    const NORMAL_KEYWORDS = [
-        // Start with '.'
-        'subsystem',
-        'corflags',
-        'file', 'alignment',
-        'imagebase',
-        'stackreserve',
-        'typelist',
-        'mscorlib',
-        'language',
-        'typedef',
-        'custom',
-        'module',
-        'vtfixup',
-        'vtable',
-        'namespace',
-        'class',
-        'size',
-        'pack',
-        'override',
-        'interfaceimpl',
-        'field',
-        'event',
-        'addon',
-        'removeon',
-        'fire',
-        'other',
-        'property',
-        'set',
-        'get',
-        'method',
-        'this',
-        'base',
-        'nester',
-        'permission',
-        'permissionset',
-        'line',
-        'file',
-        'hash',
-        'assembly',
-        'ver',
-        'publickey',
-        'publickeytoken',
-        'locale',
-        'mresource',
-        // Start without '.'
-        'type',
-        'enum',
-        'extern',
-        'fromunmanaged',
-        'callmostderived',
-        'retainappdomain',
-        'public',
-        'private',
-        'value',
-        'interface',
-        'sealed',
-        'abstract',
-        'auto',
-        'sequential',
-        'explicit',
-        'ansi',
-        'unicode',
-        'autochar',
         'import',
-        'serializable',
-        'windowsruntime',
+        'in',
+        'initonly',
+        'instance',
+        'interface',
+        'internalcall',
+        'lasterr',
+        'legacy',
+        'library',
+        'literal',
+        'managed',
+        'marshal',
         'nested',
-        'beforefieldinit',
-        'specialname',
+        'newslot',
+        'noinlining',
+        'nomangle',
+        'nometadata',
+        'nooptimization',
+        'noplatform',
+        'off',
+        'on',
+        'optil',
+        'opt',
+        'out',
+        'pinvokeimpl',
+        'preservesig',
+        'private',
+        'privatescope',
+        'public',
+        'reqsecobj',
+        'retargetable',
         'rtspecialname',
-        'flags',
+        'runtime',
+        'sealed',
+        'sequential',
+        'serializable',
+        'specialname',
+        'static',
+        'stdcall',
+        'strict',
+        'synchronized',
+        'thiscall',
+        'unmanaged',
+        'unmanagedexp',
+        'unicode',
+        'value',
+        'valuetype',
+        'vararg',
+        'virtual',
+        'winapi',
+        'windowsruntime',
+        'x86'
+    ];
+
+    const ATTR_EXTRA_KEYWORDS = [
         'extends',
         'implements',
-        'param',
-        'static',
-        'family',
-        'initonly',
-        'marshal',
-        'famandassem',
-        'famorassem',
-        'privatescope',
-        'literal',
-        'notserialized',
-        'at',
-        'mdtoken',
-        'final',
-        'virtual',
-        'strict',
-        'hidebysig',
-        'newslot',
-        'unmanagedexp',
-        'reqsecobj',
-        'pinvokeimpl',
+        'with',
+        'to',
+        'handler'
+    ];
+
+    const LITERAL_KEYWORDS = [
+        'false',
+        'null',
+        'nullref',
+        'true'
+    ];
+
+    const CONTEXTUAL_KEYWORDS = [
+        '.ctor',
+        '.cctor',
+        'init',
         'as',
-        'nomangle',
-        'lasterr',
-        'winapi',
-        'bestfit',
-        'on',
-        'off',
-        'charmaperror',
-        'in',
-        'out',
-        'opt',
-        'native',
-        'cil',
-        'optil',
-        'managed',
-        'forwardref',
-        'preservesig',
-        'runtime',
-        'internalcall',
-        'synchronized',
-        'noinlining',
-        'aggressiveinlining',
-        'nooptimization',
-        'tls', 'request',
-        'demand',
+        'method',
+        'type',
+        'constraint',
+        'catch',
+        'filter',
+        'finally',
+        'fault',
+        'tls',
+    ];
+
+    const META_KEYWORDS = [
+        'define',
+        'else',
+        'endif',
+        'ifdef',
+        'ifndef',
+        'include',
+        'line',
+        'undef'
+    ];
+
+    const OTHER_KEYWORDS = [
+        '.this',
+        '.base',
+        '.nester',
+        'algorithm',
+        'alignment',
+        'at',
+        'bytearray',
+        'extern',
+        'modreq',
+        'modopt',
+        'field',
+        'fixed',
+        'notserialized',
+        'any',
         'assert',
+        'callmostderived',
+        'demand',
         'deny',
-        'permitonly',
-        'linkcheck',
+        'fromunmanaged',
         'inheritcheck',
+        'linkcheck',
+        'mdtoken',
+        'noncasdemand',
+        'noncasinheritance',
+        'noncaslinkdemand',
+        'permitonly',
+        'prejitdeny',
+        'prejitgrant',
+        'property',
         'reqmin',
         'reqopt',
         'reqrefuse',
-        'prejitgrant',
-        'prejitdeny',
-        'noncasdemand',
-        'noncaslinkdemand',
-        'noncasinheritance',
-        'retargetable',
-        'legacy', 'library',
-        'x86',
-        'ia64',
-        'amd64',
-        'arm',
-        'void'
+        'request',
+        'retainappdomain',
     ];
-    const LITERAL_KEYWORDS = [
-        'default',
-        'nullref',
+
+    const CLASS_MODIFIERS = [
+        ATTR_KEYWORDS[0],   // abstract
+        ATTR_KEYWORDS[4],   // ansi
+        ATTR_KEYWORDS[7],   // assembly
+        ATTR_KEYWORDS[9],   // auto
+        ATTR_KEYWORDS[10],  // autochar
+        ATTR_KEYWORDS[11],  // beforefieldinit
+        ATTR_KEYWORDS[19],  // enum
+        ATTR_KEYWORDS[20],  // explicit
+        ATTR_KEYWORDS[21],  // extended
+        ATTR_KEYWORDS[22],  // famandassem
+        ATTR_KEYWORDS[23],  // family
+        ATTR_KEYWORDS[24],  // famorassem
+        ATTR_KEYWORDS[27],  // flags
+        ATTR_KEYWORDS[31],  // import
+        ATTR_KEYWORDS[35],  // interface
+        ATTR_KEYWORDS[43],  // nested
+        ATTR_KEYWORDS[57],  // private
+        ATTR_KEYWORDS[59],  // public
+        ATTR_KEYWORDS[62],  // rtspecialname
+        ATTR_KEYWORDS[64],  // sealed
+        ATTR_KEYWORDS[65],  // sequential
+        ATTR_KEYWORDS[66],  // serializable
+        ATTR_KEYWORDS[67],  // specialname
+        ATTR_KEYWORDS[75],  // unicode
+        ATTR_KEYWORDS[76],  // value
+        ATTR_KEYWORDS[81],  // windowsruntime
+        ATTR_EXTRA_KEYWORDS[0], // extends
+        ATTR_EXTRA_KEYWORDS[1], // implements
+        ATTR_EXTRA_KEYWORDS[2], // with
+        ATTR_EXTRA_KEYWORDS[3], // to
+        ATTR_EXTRA_KEYWORDS[4]  // handler
     ];
-    const CONTEXTUAL_KEYWORDS = [
-        // Start with '.'
-        'ctor',
-        'cctor',
-        'locals',
-        'emitbyte',
-        'maxstack',
-        'entrypoint',
-        'zeroinit',
-        'export',
-        'vtentry',
-        'try',
-        'data',
-        // Start without '.'
-        'instance',
-        'callconv',
-        'vararg',
-        'unmanaged',
-        'cdecl',
-        'stdcall',
-        'thiscall',
-        'fastcall',
-        'filter',
-        'catch',
-        'finally',
-        'fault',
-        'handler'
-    ]
+
+    const METHOD_MODIFIERS = [
+        ATTR_KEYWORDS[0],   // abstract
+        ATTR_KEYWORDS[7],   // assembly
+        ATTR_KEYWORDS[22],  // famandassem
+        ATTR_KEYWORDS[23],  // family
+        ATTR_KEYWORDS[24],  // famorassem
+        ATTR_KEYWORDS[26],  // final
+        ATTR_KEYWORDS[27],  // flags
+        ATTR_KEYWORDS[30],  // hidebysig
+        ATTR_KEYWORDS[44],  // newslot
+        ATTR_KEYWORDS[55],  // pinvokeimpl
+        ATTR_KEYWORDS[57],  // private
+        ATTR_KEYWORDS[58],  // privatescope
+        ATTR_KEYWORDS[59],  // public
+        ATTR_KEYWORDS[60],  // reqsecobj
+        ATTR_KEYWORDS[62],  // rtspecialname
+        ATTR_KEYWORDS[67],  // specialname
+        ATTR_KEYWORDS[68],  // static
+        ATTR_KEYWORDS[70],  // strict
+        ATTR_KEYWORDS[71],  // synchronized
+        ATTR_KEYWORDS[74],  // unmanagedexp
+        ATTR_KEYWORDS[79]   // virtual
+    ];
+
+    const OP_CODES = [
+        // instr_none
+        'nop', 'break', 'ldarg.0', 'ldarg.1', 'ldarg.2', 'ldarg.3', 'ldloc.0', 'ldloc.1', 'ldloc.2', 'ldloc.3', 'stloc.0', 'stloc.1', 'stloc.2', 'stloc.3', 'ldnull',
+        'ldc.i4.m1', 'ldc.i4.0', 'ldc.i4.1', 'ldc.i4.2', 'ldc.i4.3', 'ldc.i4.4', 'ldc.i4.5', 'ldc.i4.6', 'ldc.i4.7', 'ldc.i4.8',
+        'dup', 'pop', 'ret',
+        'ldind.i1', 'ldind.u1', 'ldind.i2', 'ldind.u2', 'ldind.i4', 'ldind.u4', 'ldind.i8', 'ldind.i', 'ldind.r4', 'ldind.r8', 'ldind.ref',
+        'stind.ref', 'stind.i1', 'stind.i2', 'stind.i4', 'stind.i8', 'stind.r4', 'stind.r8',
+        'add', 'sub', 'mul', 'div', 'div.un', 'rem', 'rem.un', 'and', 'or', 'xor', 'shl', 'shr', 'shr.un', 'neg', 'not',
+        'conv.i1', 'conv.i2', 'conv.i4', 'conv.i8', 'conv.r4', 'conv.r8', 'conv.u4', 'conv.u8', 'conv.r.un', 'throw',
+        'conv.ovf.i1.un', 'conv.ovf.i2.un', 'conv.ovf.i4.un', 'conv.ovf.i8.un', 'conv.ovf.u1.un', 'conv.ovf.u2.un', 'conv.ovf.u4.un', 'conv.ovf.u8.un', 'conv.ovf.i.un', 'conv.ovf.u.un',
+        'ldlen', 'ldelem.i1', 'ldelem.u1', 'ldelem.i2', 'ldelem.u2', 'ldelem.i4', 'ldelem.u4', 'ldelem.i8', 'ldelem.i', 'ldelem.r4', 'ldelem.r8', 'ldelem.ref',
+        'stelem.i', 'stelem.i1', 'stelem.i2', 'stelem.i4', 'stelem.i8', 'stelem.r4', 'stelem.r8', 'stelem.ref',
+        'conv.ovf.i1', 'conv.ovf.u1', 'conv.ovf.i2', 'conv.ovf.u2', 'conv.ovf.i4', 'conv.ovf.u4', 'conv.ovf.i8', 'conv.ovf.u8',
+        'ckfinite', 'conv.u2', 'conv.u1', 'conv.i', 'conv.ovf.i', 'conv.ovf.u',
+        'add.ovf', 'add.ovf.un', 'mul.ovf', 'mul.ovf.un', 'sub.ovf', 'sub.ovf.un',
+        'endfinally', 'stind.i', 'conv.u', 'prefix7', 'prefix6', 'prefix5', 'prefix4', 'prefix3', 'prefix2', 'prefix1', 'prefixref',
+        'arglist', 'ceq', 'cgt', 'cgt.un', 'clt', 'clt.un', 'localloc', 'endfilter', 'volatile.', 'tail.', 'cpblk', 'initblk', 'rethrow', 'refanytype', 'readonly.', 'illegal', 'endmac',
+        // instr_var
+        'ldarg.s', 'ldarga.s', 'starg.s', 'ldloc.s', 'ldloca.s', 'stloc.s', 'ldarg', 'ldarga', 'starg', 'ldloc', 'ldloca', 'stloc',
+        // instr_i
+        'ldc.i4.s', 'ldc.i4', 'unaligned.', 'no.',
+        // instr_i8
+        'ldc.i8',
+        // instr_r
+        'ldc.r4', 'ldc.r8',
+        // instr_brtarget
+        'br.s', 'brfalse.s', 'brtrue.s', 'beq.s', 'bge.s', 'bgt.s', 'ble.s', 'blt.s', 'bne.un.s', 'bge.un.s', 'bgt.un.s', 'ble.un.s', 'blt.un.s',
+        'br', 'brfalse', 'brtrue', 'beq', 'bge', 'bgt', 'ble', 'blt', 'bne.un', 'bge.un', 'bgt.un', 'ble.un', 'blt.un', 'leave', 'leave.s',
+        // instr_method
+        'jmp', 'call', 'callvirt', 'newobj', 'ldftn', 'ldvirtftn',
+        // instr_field
+        'ldfld', 'ldflda', 'stfld', 'ldsfld', 'ldsflda', 'stsfld',
+        // instr_type
+        'cpobj', 'ldobj', 'castclass', 'isinst', 'unbox', 'stobj', 'box', 'newarr', 'ldelema', 'ldelem', 'stelem', 'unbox.any', 'refanyval', 'mkrefany', 'initobj', 'constrained.', 'sizeof',
+        // instr_string
+        'ldstr',
+        // instr_sig
+        'calli',
+        // instr_tok
+        'ldtoken',
+        // instr_switch
+        'switch'
+    ];
 
     const KEYWORDS = {
-        keyword: NORMAL_KEYWORDS.concat(CONTEXTUAL_KEYWORDS),
-        built_in: BUILT_IN_KEYWORDS,
+        $pattern: "\\.?[a-zA-Z][\\.\\w\\d]*",
+        keyword: DECL_KEYWORDS.concat(ATTR_KEYWORDS, ATTR_EXTRA_KEYWORDS, CONTEXTUAL_KEYWORDS, OTHER_KEYWORDS),
+        built_in: BUILT_IN_KEYWORDS.concat(OP_CODES),
         literal: LITERAL_KEYWORDS
     };
     const TITLE_MODE = hljs.inherit(hljs.TITLE_MODE, { begin: '[a-zA-Z](\\.?\\w)*' });
@@ -266,11 +407,12 @@ function LanguageFn(hljs) {
         contains: [
             {
                 className: 'keyword',
-                begin: /(\\+|\\-|class|valuetype|\\.ctor)/
+                match: /(\+|-|class|valuetype|\.ctor)/
             },
             {
                 begin: '\\(',
-                end: '\\)'
+                end: '\\)',
+                keywords: KEYWORDS
             },
             TITLE_MODE
         ]
@@ -281,37 +423,22 @@ function LanguageFn(hljs) {
         aliases: [
             'il',
             'msil',
-            'ilasm'
+            'cil'
         ],
         keywords: KEYWORDS,
         contains: [
-            hljs.COMMENT(
-                '///',
-                '$',
-                {
-                    returnBegin: true,
-                    contains: [
-                        {
-                            className: 'doctag',
-                            variants: [
-                                {
-                                    begin: '///',
-                                    relevance: 0
-                                },
-                                { begin: '<!---->' },
-                                {
-                                    begin: '</?',
-                                    end: '>'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ),
             hljs.C_LINE_COMMENT_MODE,
+            hljs.C_BLOCK_COMMENT_MODE,
+            {
+                className: 'meta',
+                begin: '#',
+                end: '$',
+                keywords: { keyword: META_KEYWORDS }
+            },
             STRING,
             NUMBERS,
             {
+                className: 'class',
                 begin: '.class',
                 end: '{',
                 excludeEnd: true,
@@ -321,8 +448,9 @@ function LanguageFn(hljs) {
                         beginKeywords: CLASS_MODIFIERS.join(' '),
                         relevance: 0
                     },
+                    hljs.APOS_STRING_MODE,
                     {
-                        begin: hljs.IDENT_RE + '\\s*(<[^=]+>\\s*)?\\(',
+                        begin: hljs.IDENT_RE + '\\s*(<[^=]+>)?',
                         returnBegin: true,
                         contains: [
                             hljs.TITLE_MODE,
@@ -342,9 +470,10 @@ function LanguageFn(hljs) {
                 contains: [
                     // prevents these from being highlighted `title`
                     {
-                        beginKeywords: FUNCTION_MODIFIERS.join(' '),
+                        beginKeywords: METHOD_MODIFIERS.join(' '),
                         relevance: 0
                     },
+                    hljs.APOS_STRING_MODE,
                     {
                         begin: hljs.IDENT_RE + '\\s*(<[^=]+>\\s*)?\\(',
                         returnBegin: true,
@@ -355,6 +484,19 @@ function LanguageFn(hljs) {
                         relevance: 0
                     },
                     hljs.C_LINE_COMMENT_MODE
+                ]
+            },
+            {
+                begin: /=|bytearray\s+\(/,
+                end: '\\)',
+                excludeBegin: true,
+                excludeEnd: true,
+                keywords: KEYWORDS,
+                contains: [
+                    {
+                        className: 'number',
+                        match: /[\da-fA-F]{2}/,
+                    }
                 ]
             }
         ]
